24129e0a6ece013143ec8e4df091ea6de388e907
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-06-22 16:25:44 +0900

    added name and simple style

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  const greeting = "Hi there";
+  const myName = "Kinng";
 
   return (
   <View>
-   <Text style={styles.textStyle}>This is the components screen.</Text>
-   <Text>{greeting}</Text>
+   <Text style={{ fontSize:45 }}>Getting started with React Native!</Text>
+   <Text style={{ fontSize:20 }}>My name is {myName}</Text>
   </View> 
   );
 };
-
-const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30
-  }
-});
 
 export default ComponentsScreen;
